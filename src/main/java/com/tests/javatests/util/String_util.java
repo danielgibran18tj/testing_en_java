912c1806203ae793513c8eaff488751957116778
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-04-10 15:00:16 -0500

    aplicando diferentes tests de repeat

--- src/main/java/com/tests/javatests/util/String_util.java
+++ src/main/java/com/tests/javatests/util/String_util.java
@@ -2,6 +2,11 @@ package com.tests.javatests.util;
 
 public class String_util {
     public static String repeat(String str, int times){
+
+        if (times < 0 ){
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
--- src/test/java/com/tests/javatests/util/String_utilTest.java
+++ src/test/java/com/tests/javatests/util/String_utilTest.java
@@ -3,13 +3,32 @@ package com.tests.javatests.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+
 public class String_utilTest {
     @Test
-    public void testRepeat(){
-
-        String result = String_util.repeat("hola", 3);
-        Assert.assertEquals("holaholahola", result);
-
+    public void repeat_string_one(){
         Assert.assertEquals("hola", String_util.repeat("hola", 1));
     }
+
+    @Test
+    public void repeat_string_multiple_times(){
+        String result = String_util.repeat("hola", 3); //comando para repetir texto
+        Assert.assertEquals("holaholahola", result);  //comprobacion de igualdad
+    }
+    @Test
+    public void repeat_string_zero_time(){
+        Assert.assertEquals("", String_util.repeat("hola", 0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_time(){
+        String_util.repeat("hola", -1);
+    }
+    @Test
+    public void repeat_string2_negative_times() {
+        assertThrows(IllegalArgumentException.class , () -> {
+            String_util.repeat("hola", -1);
+        });
+    }
+    
 }
