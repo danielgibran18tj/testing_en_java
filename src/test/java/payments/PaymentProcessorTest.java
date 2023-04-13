3c75a6395aeabf578bd38dae95de9a0440020ed3
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-04-13 02:58:42 -0500

    mejora de pasarela de pagos

--- src/test/java/payments/PaymentProcessorTest.java
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -1,18 +1,24 @@
 package payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    private Payment_Gateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+    @Before  //este se ejecuta una vez antes de cada test - @BeforeClass en cambio se ejecuta una vez antes de todos los test
+    public void setup(){
+        paymentGateway = Mockito.mock(Payment_Gateway.class); //simulando el Payment_Gateway como clase
+        paymentProcessor= new PaymentProcessor(paymentGateway); //proceso de pago
+    }
     @Test
     public void payment_is_correct(){
         //PARTE 1. PREPARACION DE LOS OBJETOS
-        Payment_Gateway paymentGateway = Mockito.mock(Payment_Gateway.class); //simulando el Payment_Gateway como clase
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor= new PaymentProcessor(paymentGateway); //proceso de pago
 
         //PARTE 2. LLAMADA AL METODO QUE QUEREMOS CREAR
         boolean result = paymentProcessor.makePayment(1000);
@@ -22,10 +28,8 @@ public class PaymentProcessorTest {
     }
     @Test
     public void payment_is_wrong(){
-        Payment_Gateway paymentGateway = Mockito.mock(Payment_Gateway.class); //pasarela de pago
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor= new PaymentProcessor(paymentGateway); //proceso de pago
 
         boolean result = paymentProcessor.makePayment(1000);
         assertFalse(result);
